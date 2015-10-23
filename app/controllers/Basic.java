404ad4609ea7ad79e0019767ef4c39714707b6fe
superleo <superleo.peking@gmail.com>
2015-10-24 01:00:45 +0800

    First check-in.

--- app/controllers/Basic.java
+++ app/controllers/Basic.java
@@ -3,10 +3,14 @@ package controllers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import play.mvc.Controller;
+import play.mvc.Result;
 
 public class Basic extends Controller {
 
     final static Logger logger = LoggerFactory.getLogger(Basic.class);
 
+    public Result defaults() {
+        return ok("Works!");
+    }
 
 }
