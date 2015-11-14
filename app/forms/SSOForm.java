04af470239304974eb8a5d15dac8cf3d5845256c
superleo-cn <superleo.peking@gmail.com>
2015-11-14 12:14:36 +0800

    Create SSOForm.java

---
+++ app/forms/SSOForm.java
@@ -0,0 +1,114 @@
+package forms;
+
+public class SSOForm {
+
+    private String deviceId;
+
+    private String latitude;
+
+    private String longitude;
+
+    private String acc;
+
+    private String cellId;
+
+    private String lac;
+
+    private String plmn;
+
+    private Integer battery;
+
+    private String imsi;
+
+    private String state;
+
+    private String time;
+
+    public String getDeviceId() {
+        return deviceId;
+    }
+
+    public void setDeviceId(String deviceId) {
+        this.deviceId = deviceId;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getAcc() {
+        return acc;
+    }
+
+    public void setAcc(String acc) {
+        this.acc = acc;
+    }
+
+    public String getCellId() {
+        return cellId;
+    }
+
+    public void setCellId(String cellId) {
+        this.cellId = cellId;
+    }
+
+    public String getLac() {
+        return lac;
+    }
+
+    public void setLac(String lac) {
+        this.lac = lac;
+    }
+
+    public String getPlmn() {
+        return plmn;
+    }
+
+    public void setPlmn(String plmn) {
+        this.plmn = plmn;
+    }
+
+    public Integer getBattery() {
+        return battery;
+    }
+
+    public void setBattery(Integer battery) {
+        this.battery = battery;
+    }
+
+    public String getImsi() {
+        return imsi;
+    }
+
+    public void setImsi(String imsi) {
+        this.imsi = imsi;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
